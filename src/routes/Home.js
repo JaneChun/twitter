66e1128abb387f012f35ed7bac815109c766d9a2
JaneChun <janechun22@gmail.com>
2023-01-30 00:19:30 +0900

    feat: read tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from 'fbase';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 const Home = () => {
 	const [tweet, setTweet] = useState('');
+	const [tweets, setTweets] = useState([]);
 
+	useEffect(() => {
+		getTweets();
+	}, []);
+
+	// 트윗 불러오기
+	const getTweets = async () => {
+		const data = await getDocs(collection(db, 'Tweets'));
+		data.forEach((doc) => {
+			const tweetObject = {
+				...doc.data(),
+				id: doc.id,
+			};
+			setTweets((prev) => [tweetObject, ...prev]);
+		});
+	};
+
+	// input 핸들러
 	const onChange = (e) => {
 		const {
 			target: { value },
@@ -12,11 +30,12 @@ const Home = () => {
 		setTweet(value); // = setTweet(e.target.value);
 	};
 
+	// 트윗 추가
 	const onSubmit = async (e) => {
 		e.preventDefault();
 		try {
 			const docRef = await addDoc(collection(db, 'Tweets'), {
-				tweet,
+				text: tweet,
 				createdAt: Date.now(),
 			});
 			console.log('Document written with ID: ', docRef.id);
@@ -26,6 +45,7 @@ const Home = () => {
 
 		setTweet('');
 	};
+	console.log(tweets);
 	return (
 		<>
 			Home
@@ -33,6 +53,16 @@ const Home = () => {
 				<input value={tweet} onChange={onChange} type='text' placeholder="What's happening?" maxLength={120} />
 				<input type='submit' value='Tweet' />
 			</form>
+			<div>
+				{tweets.map((tweet) => {
+					return (
+						<div key={tweet.id}>
+							<h4>{tweet.test}</h4>
+							<span>{tweet.createdAt}</span>
+						</div>
+					);
+				})}
+			</div>
 		</>
 	);
 };
