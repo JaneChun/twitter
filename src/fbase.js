f472eaef2cd30a9c932fd9025f972eb55b3acfcf
JaneChun <janechun22@gmail.com>
2023-01-29 23:54:08 +0900

    feat: added database

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const db = getFirestore(firebase);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db } from 'fbase';
+import { collection, addDoc } from 'firebase/firestore';
 
 const Home = () => {
-	return <>Home</>;
+	const [tweet, setTweet] = useState('');
+
+	const onChange = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setTweet(value); // = setTweet(e.target.value);
+	};
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const docRef = await addDoc(collection(db, 'Tweets'), {
+				tweet,
+				createdAt: Date.now(),
+			});
+			console.log('Document written with ID: ', docRef.id);
+		} catch (error) {
+			console.error('Error adding document: ', error);
+		}
+
+		setTweet('');
+	};
+	return (
+		<>
+			Home
+			<form onSubmit={onSubmit}>
+				<input value={tweet} onChange={onChange} type='text' placeholder="What's happening?" maxLength={120} />
+				<input type='submit' value='Tweet' />
+			</form>
+		</>
+	);
 };
 
 export default Home;
